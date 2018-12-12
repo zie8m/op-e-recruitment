2d864f1f0e9dc3cf58dedeef3e73a3f9e3f3db73
zie8m <michalzielinski0@hotmail.com>
2018-12-12 19:30:07 +0000

    Clean-up

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,7 +4,6 @@ import ImageWrapper from "./ImageWrapper";
 import Image from "./Image";
 
 const Display = props => {
-  console.log(props);
   return (
     <ImageWrapper>
       <Image src={props.src} />
